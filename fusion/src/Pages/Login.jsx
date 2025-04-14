ddc52a4b7b188c3306e29cc837305e487b8e1245
KavishRajput <19021251022rajput@gmail.com>
2025-04-14 20:06:57 +0530

    add Checkbox in form

--- fusion/src/Pages/Login.jsx
+++ fusion/src/Pages/Login.jsx
@@ -1,38 +1,103 @@
 import { useState } from "react"
 
 export const Login=()=>{
-    const[username,setUsername]=useState();
-    const[password,setPassword]=useState();
+    // const[username,setUsername]=useState();
+    // const[password,setPassword]=useState();
+    const [credentials, setCredentials] = useState({ username: "", password: "" ,Gender:"",
+        hobbies:[]
+    }); // username and password must be same as name in input field
+    // const {username,password}=credentials;
+    const handlecredentials=(e)=>{
+        const {name,value,type}=e.target
+        if(type=="radio"){
+            if(e.target.checked){
+                setCredentials((prevstate)=>{
+                    return({
+                        ...prevstate,
+                        [name]:value
+                    })
+                })
+            }
+        }
+        else if(type=="checkbox"){
+            console.log("checkbox")
+            if(e.target.checked){
+                setCredentials((prevstate)=>{
+                    return({
+                        ...prevstate,
+                        [name]:[ ...prevstate.hobbies, value ]
+                        })
+                        })
+
+            }
+            else{
+                setCredentials((prevstate)=>{
+                    return({
+                        ...prevstate,
+                        [name]:prevstate.hobbies.filter((v)=>value!==v)
+                        })
+                        })
+            }
+        }
+
+        // console.log(name,value)
+        else{
+        setCredentials((prevstate)=>{
+            return({
+                ...prevstate,
+                [name]:value
+            })
+        })
+    }
+
+        console.log(credentials)
+    }
     const loginsubmit=(e)=>{
         e.preventDefault();
-        console.log(username,password);
+        console.log(credentials);
     }
     return (
         <>
         <div>
             <h1> Login Page</h1>
             <form action={""} method="POST" onSubmit={loginsubmit}>
-                <label>Username:</label>
-                <br />
-                <input type="text" name="username" className="border-2" value={username} onChange={(e)=>setUsername(e.target.value)}/>
+                {/* Username Start */}
+                            <label>Username:</label>
+                            <br />
+                            <input type="text" name="username" className="border-2" 
+                            // value={username}
+                             onChange={
+                                // (e)=>setUsername(e.target.value)
+                                handlecredentials
+                                }/>
+                {/* Username End */}
                 <br/>
-                <label>Password:</label>
-                <br />
-                <input type="password" name="password" className="border-2" value={password} onChange={(e)=>setPassword(e.target.value)}/>
+                {/* Password Start */}
+                            <label>Password:</label>
+                            <br />
+                            <input type="password" name="password" className="border-2" 
+                            // value={password}
+                             onChange={
+                                // (e)=>setPassword(e.target.value)
+                                handlecredentials
+                                }/>
+                {/* Password End */}
                 <br/>
-                <h2>Gender</h2>
-                <input type="radio" name="Gender" id="male" />
-                <label for="male">Male</label>
-                <input type="radio" name="Gender" id="female" />
-                <label for="female">Female</label>
+                {/* Gender Start */}
+                            <h2>Gender</h2>
+                            <input type="radio" name="Gender" id="male" value={"male"} onChange={handlecredentials}/>
+                            <label htmlFor="male">Male</label>
+                            <input type="radio" name="Gender" id="female" value={"female"} onChange={handlecredentials}/>
+                            <label htmlFor="female">Female</label>
+                {/* Gender End */}
                 <br/>
                 <h2>Hobbies</h2>
-                <input type="checkbox" name="hobbies" id="reading" />
-                <label for="reading">Reading</label>
-                <input type="checkbox" name="hobbies" id="swimming" />
-                <label for="swimming">Swimming</label>
-                <input type="checkbox" name="hobbies" id="dancing" />
-                <label for="dancing">Dancing</label>
+                <input type="checkbox" name="hobbies" id="reading" value={"reading"} onChange={handlecredentials}/>
+                <label htmlFor="reading">Reading</label>
+                <input type="checkbox" name="hobbies" id="swimming" value={"swimming"} onChange={handlecredentials}/>
+                <label htmlFor="swimming">Swimming</label>
+                <input type="checkbox" name="hobbies" id="dancing" value={"dancing"} onChange={handlecredentials}/>
+                <label htmlFor="dancing">Dancing</label>
                 <br />
                 <button type="submit">Login</button>
             </form>
