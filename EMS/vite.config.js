f2d936d8e3333243c3c7be8964997b6509a53854
KavishRajput <19021251022rajput@gmail.com>
2025-07-24 22:57:31 +0530

    edit viteconigefile

--- EMS/vite.config.js
+++ EMS/vite.config.js
@@ -5,4 +5,11 @@ import tailwindcss from '@tailwindcss/vite'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(),tailwindcss()],
+  base: "/", // important if deployed at root
+  build: {
+  outDir: "dist",
+  },
+  server: {
+    historyApiFallback: true, // <-- important for dev server too
+  },
 })
